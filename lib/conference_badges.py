09ca6a42e546decfd017e948c55a52e81c00e4ec
Kelvin okumba <okumbakelvin@gmail.com>
2023-10-10 15:44:27 +0300

    added

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,28 @@
-def badge_maker(name):
-    return None
+def badge_maker(name=""):
+    return (f"Hello, my name is {name}")
+badge_maker()
 
 def batch_badge_creator(names):
-    return None
+    return [badge_maker(name) for name in names]
+
 
 def assign_rooms(names):
-    return None
+    rooms = range(1, len(names) + 1)
+    names = []
+
+    for i, speaker in enumerate(names):
+        names = f"Hello, {speaker}! You'll be assigned to room {rooms[i]}."
+        names.append(names)
+
+    return names
 
 def printer(names):
-    return None
+   # Get badge messages
+    badges = batch_badge_creator(names)
+    for badge in badges:
+        print(badge)
+
+    # Get room assignment messages
+    room_assignments = assign_rooms(names)
+    for assignment in room_assignments:
+        print(assignment)
